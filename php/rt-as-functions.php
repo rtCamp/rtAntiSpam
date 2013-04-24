977197e33990e265d0b8a94a8b982b08cb13ef3e
Joshua Abenazer <joshua.abenazer@rtcamp.com>
2013-04-24 10:49:32 +0530

    Added Support for buddypress register form

--- php/rt-as-functions.php
+++ php/rt-as-functions.php
@@ -402,3 +402,46 @@ function rtas_get_feeds( $feed_url='http://feeds.feedburner.com/rtpanel' ) {
         } ?>
     </ul><?php
 }
+
+function rtas_bp_add_code() {
+	global $bp;
+	$theme = 'white'; // possible values: 'red', 'white', 'blackglass', 'clean' ( http://code.google.com/intl/de-DE/apis/recaptcha/docs/customization.html )
+        $lang = 'en'; // possibble values: 'en', 'nl', 'fr', 'de', 'pt', 'ru', 'es', 'tr' ( http://code.google.com/intl/de-DE/apis/recaptcha/docs/customization.html )
+	$script = "<script type=\"text/javascript\">
+ 			var RecaptchaOptions = {
+    			theme : '".$theme."',
+				lang: '".$lang."'
+ 			};
+		</script>";
+		
+	$html = '<div class="register-section" id="security-section">';
+	$html .= '<div class="editfield">';
+	$html .= $script;
+	$html .= '<label>CAPTCHA code</label>';
+	if (!empty($bp->signup->errors['recaptcha_response_field'])) {
+		$html .= '<div class="error">';
+		$html .= $bp->signup->errors['recaptcha_response_field'];
+		$html .= '</div>';
+	}
+	$html .= recaptcha_get_html(RTAS_PUBLIC_KEY);
+	$html .= '</div>';
+	$html .= '</div>';
+	echo $html;
+}
+
+function rtas_bp_validate($errors) {
+	global $bp;
+        $strError = __('Please check the CAPTCHA code. It\'s not correct.');
+	if (function_exists('recaptcha_check_answer')) {
+		$response = recaptcha_check_answer(RTAS_PRIVATE_KEY, $_SERVER['REMOTE_ADDR'], $_POST['recaptcha_challenge_field'], $_POST['recaptcha_response_field']);
+
+		if (!$response->is_valid) {
+			$bp->signup->errors['recaptcha_response_field'] = $strError;
+		}
+	}
+
+	return;
+}
+
+add_action( 'bp_before_registration_submit_buttons', 'rtas_bp_add_code' );
+add_action( 'bp_signup_validate', 'rtas_bp_validate' );
